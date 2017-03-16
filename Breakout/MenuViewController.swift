dce47be74f1f3f399753c6c547a539100c2b0dcd
BrianThyfault <student@bthyfault1.local>
2017-03-16 11:11:15 -0500

    1

---
+++ Breakout/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuViewController.swift
+//  Breakout
+//
+//  Created by Brian W Thyfault on 3/16/17.
+//  Copyright © 2017 Brian W Thyfault. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
